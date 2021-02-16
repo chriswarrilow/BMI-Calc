daf38bc7f59b5d198503ae4757806c87e38b7621
Chris Warrilow <meth0d@comcast.net>
2021-02-16 06:26:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# 🚨 Don't change the code below 👇
+#height = input("enter your height in inches: ")
+weight = input("enter your weight in lbs: ")
+height = input("enter your height in inches: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+bmiWeight = (int(weight)) * 705
+susanHeight = (int(height)) 
+bmiHeight = bmiWeight / susanHeight
+bmiTotal = (int(bmiHeight)) / susanHeight
+
+#print(type(bmiWeight, bmiHeight, bmiTotal))
+print(round(bmiTotal))
+#print(bmiWeight)
+#print(type(susanHeight))
+#print(bmiHeight)
+#print(type(bmiHeight))
+#print(bmiHeight / bmiHeight)
+
+
+
+
+
+
+
