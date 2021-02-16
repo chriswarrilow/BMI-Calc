a1125f6a5f49ec7ac91aec1b1ed45e2ca6fc2edd
meth0d1244 <50190911+meth0d1244@users.noreply.github.com>
2021-02-15 22:45:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-# 🚨 Don't change the code below 👇
-#height = input("enter your height in inches: ")
 weight = input("enter your weight in lbs: ")
 height = input("enter your height in inches: ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
 bmiWeight = (int(weight)) * 705
 susanHeight = (int(height)) 
 bmiHeight = bmiWeight / susanHeight
